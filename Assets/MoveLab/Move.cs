27f562784c72add6b75e4d535224b2ac332e7c53
Emeric Coude <emericmax9@hotmail.com>
2022-09-13 21:24:02 -0400

    Lab

---
+++ Assets/MoveLab/Move.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    [Header("General")]
+    [SerializeField] private Transform _goal;
+    [SerializeField] private float _speed = 3f;
+    
+    enum MoveType{MoveTowards, Translate, Lerp}
+    [SerializeField] private MoveType _moveType = MoveType.MoveTowards;
+
+    [Header("Translate")]
+    [SerializeField] private float _maxDistance = 0.1f;
+
+    void Update() {
+        transform.LookAt(_goal);
+        Vector3 direction = _goal.position - transform.position;
+
+        switch (_moveType)
+        {
+            case MoveType.MoveTowards:
+                transform.position = Vector3.MoveTowards(transform.position, _goal.position, _speed * Time.deltaTime);
+                break;
+            
+            case MoveType.Translate:
+                if (Vector3.Distance(transform.position, _goal.position) > _maxDistance)
+                    transform.Translate(direction.normalized * _speed * Time.deltaTime);
+                break;
+
+            case MoveType.Lerp:
+                if (Vector3.Distance(transform.position, _goal.position) > _maxDistance)
+                    transform.position = Vector3.Lerp(transform.position, _goal.position, _speed * Time.deltaTime);
+                break;
+        }
+    }
+}
